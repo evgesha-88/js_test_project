5e1f9a983f7c515a15d8cb20364f0ba87a48b5c0
evgesha-88 <vlorien_2011@mail.ru>
2020-11-17 20:46:46 +0300

    Added new version

---
+++ index.js
@@ -0,0 +1,44 @@
+"use strict";
+let num = 3;
+alert(num);
+
+let a = 10;
+let b = 2;
+alert(a+b);
+alert(a-b);
+alert(a/b);
+alert(a*b);
+
+let c = 15;
+let d = 2;
+let result = c+d;
+alert(result);
+
+let a = 10;
+let b = 2;
+let c = 5;
+alert(a+b+c);
+
+let a = 17;
+let b = 10;
+let c = (a-b);
+let d = 7;
+let result = c+d;
+alert (result);
+
+let str = "Привет, Мир!";
+alert(str);
+
+let str1 = "Привет";
+let str2 = "Мир!"
+alert(str1+","+str2);
+
+let name = "Evgeniya";
+alert(`Привет, ${name}!`);
+
+let age = 31;
+alert(`Мне ${age} год!`);
+
+
+
+
